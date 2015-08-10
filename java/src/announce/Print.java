fe41ee21a40e6564bd3fcace46933e4f4cea3dba
quanlieu <quanlieu@kms-technology.com>
2015-08-10 16:04:32 +0700

    First java code

---
+++ java/src/announce/Print.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package announce;
+
+/**
+ * @author quanlieu
+ *
+ */
+public class Print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st line");
+	}
+
+}
