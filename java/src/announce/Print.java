87e418d2034369d1e5ab562b2a3cfdafd1cc5bae
quanlieu <quanlieu@kms-technology.com>
2015-08-10 16:33:01 +0700

    2nd line

--- java/src/announce/Print.java
+++ java/src/announce/Print.java
@@ -14,8 +14,8 @@ public class Print {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1st line.");
-		System.out.println("2nd line.");
+		System.out.println("1st line");
+		System.out.println("2nd line");
 	}
 
 }
