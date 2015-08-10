17f365965eeadbcddc2ea87d74dca4eb546bd8be
quanlieu <quanlieu@kms-technology.com>
2015-08-10 16:58:29 +0700

    delete 1 line

--- java/src/announce/Print.java
+++ java/src/announce/Print.java
@@ -14,7 +14,6 @@ public class Print {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1st line");
 		System.out.println("2nd line");
 	}
 
